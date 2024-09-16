57d2db53b310f480946633f76c68cb2857ca5646
Farhan Chowdury <fchowdury@brooklynsteamcenter.org>
2024-09-16 10:52:14 -0400

    Initial lesson

---
+++ lesson_00.py
@@ -0,0 +1,30 @@
+# Step 1: Create a file and name it
+# Step 2: Print name
+print("Farhan Chowdury ")
+# Step 3: Create var "fav show" storing name of fav show
+favorite_show = "South Park"
+# Step 4: Use snake case to create ONE float and integer
+pounds = 90.5
+deaths = 127
+# Step 5: Print the product of variables
+print(pounds * deaths)
+# Step 6: Create var that stores >=3 fav foods
+foods = ["byrani, pizza, cheesecake"]
+# Step 7: Create if statement to determine drivers' license eligibility
+age = int(input("Enter your age. "))
+if age >= 16:
+    print("You can get a driver's license! Hooray! ")
+else:
+    print("You are NOT eligible for a driver's license. Womp womp. ")
+# Step 8: Ask for name, print to console
+name = input("What is your name? ")
+print(name)
+# Step 9: Create a for loop printing each of fav food
+for x in foods:
+    print(x)
+    if x == "pizza":
+        print("Mamma mia ")
+# Step 10: Create a while loop that never runs
+while age >=18:
+    print("UNC UNC UNC ")
+    age +=18
