31de595d6dbe40f0b017b7e02b40da3f258f98be
Farhan Chowdury <fchowdury@brooklynsteamcenter.org>
2024-09-18 10:50:46 -0400

    Second lesson on formatting
    Due tomorrow Sep 19, but will be finished home

---
+++ 02_formatting.py
@@ -0,0 +1,65 @@
+# Task 1: Check if user inputted correct pwd (NOT case sensitive)
+# Define Function = check
+# Variable = password
+def check():
+        # User inputs password
+        password = str(input("Enter your password: "))
+        # Check for validity + convert all characters to lowercase
+        if password.lower() == "password":
+            print("Correct password! Congratulations!")
+        # Print incorrect and rerun func
+        else:
+            print("Incorrect Password! Try again.")
+            check()
+check()
+print("")
+
+# Task 2: Check if user input is empty. Print result based on input
+# Define function = emptiness
+# Variable = empty_string
+def emptiness():
+    #  User inputs empty string or any characters
+    empty_string = str(input("Enter something: "))
+    # Check if string is empty. Print invalid if so
+    if empty_string == "":
+         print("Invalid input!")
+    # If string contains any characters
+    else:
+         print("Valid input!") 
+emptiness()
+print("")
+
+# Task 3: User inputs sentence with the word "cat" used multiple times throughout. Each "cat" is replaced with "dog" regardless of capitalization
+# Define function = dog_sentence
+# Variable = cat_sentence
+def dog_sentence():
+     # User inputs sentence with "cat"
+     cat_sentence = str(input("Enter a sentence using the word cat AT LEAST 3 times (where capitlizaed or not): "))
+    # Replace all "cat" words with "dog" words
+
+# Task 4
+
+# Task 5
+
+
+# """
+# TASK 3:
+
+# Write a program that will replace the word "cat" with the word "dog"
+# It should replace all occurances regardless of captilization 
+# """
+
+
+# """
+# TASK 4:
+
+# Write a program that takes a person's name and age as input and prints it
+# """
+
+
+# """
+# TASK 5:
+
+# Write a program that takes in two floats, and prints the quotient
+# The result should be rounded to the nearest tenth (1 decimal place)
+# """
